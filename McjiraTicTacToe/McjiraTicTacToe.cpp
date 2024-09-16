5147830a6d6d7b73a4b63baacafd1f878123f97f
Itamar1567 <Itamarbaharav@gmail.com>
2024-09-16 19:14:08 -0400

    Added a very hard coded WinScenario function that checks every possible win sequence on the board and outputs what type of win it is
    added a suggestion about what can be further done
    
    feel free to change code

--- McjiraTicTacToe/McjiraTicTacToe.cpp
+++ McjiraTicTacToe/McjiraTicTacToe.cpp
@@ -1,10 +1,12 @@
-#include "Declaration.h"
+#include "Player.h"
 #include <array>
+
+void WinScenario(int array[][3]);
 
 int main()
 {
 
-	InstructPlayer();
+	//InstructPlayer();
 
 	// int for column lookup(j = rows, i = columns
 	int i = 0;
@@ -14,49 +16,118 @@ int main()
 	//To access location you must choose the x,y positions
 	int board[3][3] = {
 
-		{ -1, -1, -1 },
-		{ -1, -1, -1 },
-		{ -1, -1, -1 }
+		{ 0, 0, 1 },
+		{ -1, 0, 0 },
+		{ 1, 0, 1 }
 
 	};
-
+	/*
+	* 
+	* Suggestion: 
+	* 
+	* While(bool for win scenario)
+	* {
+	* 
+	*		- this will have to be proben into turns
+	*		-
+	*		- std::cin << cordinate
+    *		
+	*     winScenario(board); to check for a win 
+	* 
+	* }
+	*/
 	Player player_1;
 	Player player_2;
 
 
-
+	WinScenario(board);
 
 
 	//Bool for alternating turns
 	bool turn = false;
 
-	// for loop to check iterate through the board
-		//use to check where X or O is placed
-	for (int j = 0; j < 3; j++)
+	
+}
+
+void WinScenario(int array[][3])
+{
+
+	//down lines 
+	
+		//Checks for down line from on column 1
+
+	if (array[0][0] == array[1][0] && array[1][0] == array[2][0] && array[2][0] != -1)
 	{
+		std::cout << array[0][0] << " Wins!!! by a vertical line on column 1";
+		return;
+	}
+		//Checks for down line from on column 2
 
-		if (i == 3)
-		{
-			break;
-		}
+	if (array[0][1] == array[1][1] && array[1][1] == array[2][1] && array[2][1] != -1)
+	{
+		std::cout << array[0][1] << " Wins!!! by a vertical line on column 2";
+		return;
+	}
 
-		else if (j == 2)
-		{
-			i++;
+		//Checks for down line from on column 3
 
-			// -1 due to incrementation after loop end
-			j = -1;
-		}
+	if (array[0][2] == array[1][2] && array[1][2] == array[2][2] && array[2][2] != -1)
+	{
+		std::cout << array[0][2] << " Wins!!! by a vertical line on column 3";
+		return;
+	}
 
-		std::cout << board[i][j] << std::endl;
+	// Checking Diagonals
 
+		//top left to bottom right
+	if (array[0][0] == array[1][1] && array[1][1] == array[2][2] && array[2][2] != -1)
+	{
+		std::cout << array[0][0] << " Wins!!! by a top left to bottom right diagonal creation";
+		return;
+	}
+		//top right to bottom left
 
+	if (array[0][2] == array[1][1] && array[1][1] == array[2][0] && array[2][0] != -1)
+	{
+		std::cout << array[0][2] << " Wins!!! by a top right to bottom left diagonal creation";
+		return;
+	}
 
+	// straight line
 
+		//top line
+	if (array[0][0] == array[0][1] && array[0][1] == array[0][2] && array[0][2] != -1)
+	{
+		std::cout << array[0][0] << " Wins!!! by top line creation";
+		return;
+	}
 
+		//middle line
+	if (array[1][0] == array[1][1] && array[1][1] == array[1][2] && array[1][2] != -1)
+	{
+		std::cout << array[1][0] << " Wins!!! by middle line creation";
+		return;
+	}
+
+		//bottom line
+	if (array[2][0] == array[2][1] && array[2][1] == array[2][2] && array[2][2] != -1)
+	{
+		std::cout << array[2][0] << " Wins!!! by bottom line creation";
+		return;
+	}
+
+	else
+	{ 
+	
+		std::cout << "Next turn";
+		//continue loop for next turn
 
 	}
+
+	
 }
+
+
 
 void DrawBoard()
 {
@@ -82,24 +153,28 @@ void DrawBoard()
 
 }
 
+/*
+
 void InstructPlayer()
 {
-	cout << "*****Tic Tac Toe *****" << endl;
-	cout << endl;
-	cout << "Directions: Choose which box you want to alter!\n" << endl;
+	std::cout << "*****Tic Tac Toe *****" << endl;
+	std::cout << endl;
+	std::cout << "Directions: Choose which box you want to alter!\n" << endl;
 
-	cout << " 0,0 | 0,1 | 0,2 " << endl;
-	cout << "-----------------" << endl;
-	cout << " 1,0 | 1,1 | 1,2 " << endl;
-	cout << "-----------------" << endl;
-	cout << " 2,0 | 2,1 | 2,2 \n" << endl;
+	std::cout << " 0,0 | 0,1 | 0,2 " << endl;
+	std::cout << "-----------------" << endl;
+	std::cout << " 1,0 | 1,1 | 1,2 " << endl;
+	std::cout << "-----------------" << endl;
+	std::cout << " 2,0 | 2,1 | 2,2 \n" << endl;
 
-	cout << "Player " << playerturn << ", please choose an option!\n" << endl;
-	cout << "X value : "<< endl;
-	cin >> xvalue;
-	cout << "Y value :" << endl;
-	cin >> yvalue;
+	std::cout << "Player " << playerturn << ", please choose an option!\n" << endl;
+	std::cout << "X value : "<< endl;
+	std::cin >> xvalue;
+	std::cout << "Y value :" << endl;
+	std::cin >> std::yvalue;
 }
+
+*/
 
 void ResetBoard()
 {
