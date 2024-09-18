cd780a8aaa57a2c973ecd170eeeb060408d05b9c
ryane110 <ryanelbassiouny11@outlook.com>
2024-09-18 17:27:35 -0400

    Added Reset Board function

--- McjiraTicTacToe/McjiraTicTacToe.cpp
+++ McjiraTicTacToe/McjiraTicTacToe.cpp
@@ -120,6 +120,7 @@ bool CheckBoard(char array[][3])
 }
 
 void displayBoard(char board[3][3]) {
+	cout << "\n";
 	cout << board[0][0]<<"|"<<board[0][1]<<"|"<<board[0][2] << endl;
 	cout << "-----" << endl;
 	cout << board[1][0] << "|" << board[1][1] << "|" << board[1][2] << endl;
@@ -189,6 +190,18 @@ char InstructPlayer(char board[3][3], bool turn)
 
 }
 
+//Function to reset the board
+void ResetBoard(char board[3][3])
+{
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			board[i][j] = '-';
+		}
+	}
+}
+
 	int main()
 	{
 
@@ -217,7 +230,7 @@ char InstructPlayer(char board[3][3], bool turn)
 
 		}
 		
-
+		ResetBoard(board);
 	};
 
 	//Player player_1;
@@ -282,12 +295,7 @@ void InstructPlayer()
 */
 
 
-//Function to reset the board
-void ResetBoard()
-{
 
-
-}
 
 
 
